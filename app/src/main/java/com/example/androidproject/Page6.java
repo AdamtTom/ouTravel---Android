e9c0c0f120f9afa3a7c6c38bcbba4d1364a5a26e
naomi-byunn <naomibyun7@gmail.com>
2022-10-13 01:54:31 -0700

    added page6 and text placeholders will be replaced by the extracted data from open source data in the future

---
+++ app/src/main/java/com/example/androidproject/Page6.java
@@ -0,0 +1,21 @@
+package com.example.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Page6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Button button = findViewById(R.id.btnToPage6);
+//        Button.setOnClickListener(view -> {
+//            Intent intent = new Intent(this, Page5.class);
+//            startActivity(intent);
+//        });
+    }
+}
