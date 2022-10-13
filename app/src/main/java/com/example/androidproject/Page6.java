454cb8187e045e6969c2b979f2ca3bb9ca39fcb0
naomi-byunn <naomibyun7@gmail.com>
2022-10-13 14:28:20 -0700

    updated variable names for string and id to distinguish they are page6 properties

--- app/src/main/java/com/example/androidproject/Page6.java
+++ app/src/main/java/com/example/androidproject/Page6.java
@@ -12,9 +12,9 @@ public class Page6 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page6);
 
-//        Button button = findViewById(R.id.btnToPage6);
+//        Button button = findViewById(R.id.page6_button);
 //        Button.setOnClickListener(view -> {
-//            Intent intent = new Intent(this, Page5.class);
+//            Intent intent = new Intent(this, Page7.class);
 //            startActivity(intent);
 //        });
     }
