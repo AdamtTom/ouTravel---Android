d89a41221c8a242ab6f43ba498c7405cfb6f25a5
naomi-byunn <naomibyun7@gmail.com>
2022-10-13 18:33:34 -0700

    added fragment in page6

--- app/src/main/java/com/example/androidproject/Page6.java
+++ app/src/main/java/com/example/androidproject/Page6.java
@@ -1,8 +1,10 @@
 package com.example.androidproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 
 public class Page6 extends AppCompatActivity {
@@ -11,6 +13,12 @@ public class Page6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page6);
+
+        Page6Fragment fragment1 = new Page6Fragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.linear_layout, fragment1).commit();
+
 
 //        Button button = findViewById(R.id.page6_button);
 //        Button.setOnClickListener(view -> {
