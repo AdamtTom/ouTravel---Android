a29860a2b264e8390ace173607fc9369bb800bf3
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2022-11-06 00:11:31 -0700

    added sample API call in MainActivity.java and added necessary dependencie to build.gradle

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -6,15 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.android.volley.AuthFailureError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference databaseReference;
     FirebaseDatabase firebaseDatabase;
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "fa211ad253385ab5e5f303af6dfebb44";
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +48,69 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.startBtn);
         btn.setOnClickListener(v -> start());
+
+        tvResult = findViewById(R.id.textView2);
+        String cityName = "Toronto";
+        String tempUrl = url + "?q=" + cityName + "&appid=" + appid;
+
+        AsyncTaskRunner runner = new AsyncTaskRunner();
+        runner.execute(tempUrl);
+
+    }
+
+    private class AsyncTaskRunner extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, strings[0], null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+                        JSONObject jsonObjectMain = response.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                        double feelslike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                        int pressure = jsonObjectMain.getInt("pressure");
+                        int humidity = jsonObjectMain.getInt("humidity");
+
+                        JSONArray weather = response.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = weather.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+
+                        JSONObject jsonObjectWind = response.getJSONObject("wind");
+                        double speed = jsonObjectWind.getDouble("speed");
+                        int degree = jsonObjectWind.getInt("deg");
+
+                        JSONObject jsonObjectClouds = response.getJSONObject("clouds");
+                        int cloud = jsonObjectClouds.getInt("all");
+
+                        JSONObject jsonObjectSys = response.getJSONObject("sys");
+                        String currentCountry = jsonObjectSys.getString("country");
+                        String currentCity = response.getString("name");
+
+                        tvResult.setText("Current weather of " + currentCity + " (" + currentCountry + ")\n"
+                                + "Temperature: " + temp + " \u2103\n"
+                                + "Feels like: " + feelslike + " \u2103\n"
+                                + "Humidity: " + humidity + "%\n"
+                                + "Description: " + description + "\n"
+                                + "Wind speed: " + speed + "\n"
+                                + "Wind degree: " + degree + "\n"
+                                + "Cloudiness: " + cloud + "%\n"
+                                + "Pressure: " + pressure + " hPa");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            queue.add(request);
+            return null;
+        }
     }
 
     private void addData(){
@@ -44,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
         String id = databaseReference.push().getKey();
         databaseReference.child("City").child(id).setValue(vancouver);
     }
+
 
     public void start(){
         Intent intent = new Intent(this, Page2.class);
